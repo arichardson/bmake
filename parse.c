c1fd43385f22143a8694558f6aa9d240c2537405
T Ar Creator <tar@example.com>
2011-02-26 01:49:38 +0000

    Imported from bmake-20110225.tar.gz.

--- parse.c
+++ parse.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: parse.c,v 1.173 2010/12/25 21:39:11 dholland Exp $	*/
+/*	$NetBSD: parse.c,v 1.176 2011/02/20 23:12:09 joerg Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1993
@@ -69,14 +69,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: parse.c,v 1.173 2010/12/25 21:39:11 dholland Exp $";
+static char rcsid[] = "$NetBSD: parse.c,v 1.176 2011/02/20 23:12:09 joerg Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)parse.c	8.3 (Berkeley) 3/19/94";
 #else
-__RCSID("$NetBSD: parse.c,v 1.173 2010/12/25 21:39:11 dholland Exp $");
+__RCSID("$NetBSD: parse.c,v 1.176 2011/02/20 23:12:09 joerg Exp $");
 #endif
 #endif /* not lint */
 #endif
@@ -2217,7 +2217,7 @@ ParseSetParseFile(const char *filename)
 
     slash = strrchr(filename, '/');
     if (slash == NULL) {
-	Var_Set(".PARSEDIR", ".", VAR_GLOBAL, 0);
+	Var_Set(".PARSEDIR", curdir, VAR_GLOBAL, 0);
 	Var_Set(".PARSEFILE", filename, VAR_GLOBAL, 0);
     } else {
 	len = slash - filename;
@@ -2674,7 +2674,7 @@ ParseReadLine(void)
 		line = ParseGetLine(PARSE_RAW, &lineLength);
 		if (line == NULL) {
 		    Parse_Error(PARSE_FATAL,
-			     "Unexpected end of file in for loop.\n");
+			     "Unexpected end of file in for loop.");
 		    break;
 		}
 	    } while (For_Accum(line));
@@ -2942,7 +2942,7 @@ Parse_File(const char *name, int fd)
     if (fatals) {
 	(void)fflush(stdout);
 	(void)fprintf(stderr,
-	    "%s: Fatal errors encountered -- cannot continue\n",
+	    "%s: Fatal errors encountered -- cannot continue",
 	    progname);
 	PrintOnError(NULL, NULL);
 	exit(1);
